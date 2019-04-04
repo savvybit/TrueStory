11efcadd4132114515079f346393603884893c6a
Cosmin Poieana <cmin764@gmail.com>
2019-04-04 21:23:40 +0300

    Feature/mails (#12)
    * Fix debug page in production
    
    * Protect internal app
    
    * Properly include package data
    
    * Handle auth
    
    * Better abortion message
    
    * Save subscriber e-mails
    
    * Fix mail hashsum computation
    
    * Proper mail tests cleanup
    
    * Lower all e-mails
    
    * Add send mail capability
    
    * E-mails templates and sendgrid sending
    
    * Cleaning the code
    
    * Making sure the default value is used
    
    * Clean subscriber adding
    
    * Better code placement
    
    * Sendgrid works now
    
    * Remove unnecessary spaces from mail templates
    
    * Add confirmation e-mail
    
    * Fix mail sending images
    
    * Remove print
    
    * Updated links
    
    * Better prism
    
    * Fix gradient and add social media links
    
    * Fix text
    
    * Fix extension image
    
    * Fix fonts and extensiong image
    
    * Hotjar integration
    
    * Better hotjar integration
    
    * Add Google Analytics
    
    * Fixes after review

--- truestory/views/topics.py
+++ truestory/views/topics.py
@@ -4,9 +4,11 @@
 from flask import render_template
 
 from truestory import app
+from truestory.views import base as views_base
 
 
 @app.route("/topics")
+@views_base.require_auth
 def topics_view():
     """Topics page displaying selectable favorite news subjects."""
     return render_template("topics.html")
