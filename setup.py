b2edaef4a505f6d7e3b7961fe8f677578d667795
Cosmin Poieana <cmin764@gmail.com>
2019-06-23 23:45:33 +0300

    Fix gitlab requirement

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ def get_requirements(path="requirements.txt"):
 
 setup(
     name="truestory",
-    version="0.7.1",
+    version="0.7.2",
     description="Be your own journalist.",
     long_description=read("README.md"),
     url="https://gitlab.com/truestory-one/TrueStory",
