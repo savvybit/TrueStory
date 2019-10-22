73411a456bf5e06d4e43c908399def74c0685309
Cosmin Poieana <cmin764@gmail.com>
2019-10-22 09:15:08 +0300

    Fix setup, version and installation file inclusion

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python3
 
+"""Installs/develops found packages along their requirements."""
 
-import os
+
 from pathlib import Path
 
 from setuptools import find_packages, setup
@@ -14,15 +15,12 @@ def read(given_path):
     """Returns the striped content of a file."""
     given_path = Path(given_path)
     relative_path = Path(__file__).parent / given_path
-    used_path = relative_path
+    used_path = relative_path if relative_path.is_file() else given_path
 
-    exists = lambda: os.path.isfile(used_path)
-    if not exists():
-        used_path = given_path
-        if not exists:
-            raise FileNotFoundError(
-                f"could not find any of {relative_path} or {given_path}"
-            )
+    if not used_path.is_file():
+        raise FileNotFoundError(
+            f"could not find any of {relative_path} or {given_path}"
+        )
 
     with open(used_path) as stream:
         return stream.read().strip()
@@ -37,26 +35,28 @@ def get_requirements(path="requirements.txt"):
         line = line.strip()
         if not line or any(map(line.startswith, SKIP_REQUIREMENTS)):
             continue
+
         if line.startswith("-r"):
             new_path = line[2:].strip()
             lines.extend(get_requirements(path=new_path))
             continue
+
         lines.append(line)
 
     return lines
 
 
 setup(
-    name="truestory",
-    version="0.7.2",
+    name="TrueStory",
+    version=read("VERSION"),
     description="Be your own journalist.",
     long_description=read("README.md"),
     url="https://gitlab.com/truestory-one/TrueStory",
     license="MIT",
     author="SavvyBit",
     author_email="hello@truestory.one",
-    packages=find_packages(exclude=["tests"]),
     scripts=["bin/truestory"],
+    packages=find_packages(exclude=["tests"]),
     include_package_data=True,
     zip_safe=False,
     install_requires=get_requirements(),
